4a8f81b89aec9d0fc211d38d13faf963656cea3d
Song <songzhang015@gmail.com>
2025-06-18 19:46:39 -0700

    Add global css, modify and refactor index a bit, stylize account page

--- js/account.js
+++ js/account.js
@@ -1,3 +1,20 @@
 /* JS File for Coffee Companion App - Account Page */
 
-console.log("Hello from Account!");
+function showRegister() {
+    document.querySelector(".login-form").style.display = "none";
+    document.querySelector(".register-form").style.display = "flex";
+}
+
+function showLogin() {
+    document.querySelector(".register-form").style.display = "none";
+    document.querySelector(".login-form").style.display = "flex";
+}
+
+document.querySelector(".switch-register-btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    showRegister();
+});
+document.querySelector(".switch-login-btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    showLogin();
+});
