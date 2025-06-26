e7a92c090722d4644637a3f58cacd126d5e8fa0b
Song <songzhang015@gmail.com>
2025-06-25 19:16:53 -0700

    More styling for account page, add wrapper to form to prevent header expanding outwards when switching login/register, modify HTML/JS to show only one switch button

--- js/account.js
+++ js/account.js
@@ -3,11 +3,15 @@
 function showRegister() {
     document.querySelector(".login-form").style.display = "none";
     document.querySelector(".register-form").style.display = "flex";
+    document.querySelector(".no-account").style.display = "none";
+    document.querySelector(".have-account").style.display = "block";
 }
 
 function showLogin() {
     document.querySelector(".register-form").style.display = "none";
     document.querySelector(".login-form").style.display = "flex";
+    document.querySelector(".no-account").style.display = "block";
+    document.querySelector(".have-account").style.display = "none";
 }
 
 document.querySelector(".switch-register-btn").addEventListener("click", (event) => {
