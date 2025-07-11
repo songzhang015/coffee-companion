29fbe659be6806f1f7ba0080c208d1abe31130de
Song <songzhang015@gmail.com>
2025-07-11 10:22:37 -0700

    Start navigation to log page and fix bug where event listener was placed on entire container, not icon

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,16 +1,22 @@
 /* Navigation File for Coffee Companion App to traverse between logging and adjusting brews for homescreen */
 
 function attachEventListeners() {
-	const logSection = document.querySelector(".log-container");
-	const adjustSection = document.querySelector(".adjust-container");
+	const logSection = document.querySelector(".log-container .icon-container");
+	const adjustSection = document.querySelector(
+		".adjust-container .icon-container"
+	);
 
 	logSection.addEventListener("click", () => {
-		alert("Log section clicked!");
+		navigateToLogPage();
 	});
 
 	adjustSection.addEventListener("click", () => {
 		alert("Adjust section clicked!");
 	});
+}
+
+function navigateToLogPage() {
+	document.body.innerHTML = "";
 }
 
 export { attachEventListeners };
