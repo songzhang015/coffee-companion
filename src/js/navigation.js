7045b21fcfc0e865195159d0eb6ac5defbe64cfc
Song <songzhang015@gmail.com>
2025-07-10 18:48:19 -0700

    Fix attaching listeners on log/adjust section before they were initialized

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,15 +1,15 @@
 /* Navigation File for Coffee Companion App to traverse between logging and adjusting brews for homescreen */
 
-const logSection = document.querySelector(".log-container");
-const adjustSection = document.querySelector(".adjust-container");
-
 function attachEventListeners() {
+	const logSection = document.querySelector(".log-container");
+	const adjustSection = document.querySelector(".adjust-container");
+
 	logSection.addEventListener("click", () => {
 		alert("Log section clicked!");
 	});
 
 	adjustSection.addEventListener("click", () => {
-		alert("Log section clicked!");
+		alert("Adjust section clicked!");
 	});
 }
 
