9b948f84b2dbb34ee5e2350e622686ecd89ac373
Song <songzhang015@gmail.com>
2025-09-05 16:54:15 -0700

    Restyle back button and transform adjust continue page into new color scheme

--- frontend/src/js/home.js
+++ frontend/src/js/home.js
@@ -6,18 +6,12 @@ import { getLocalEntries } from "./utils/storage";
 import { getServerEntries } from "./apis/homeApi";
 import { initMain } from "./dom/homeUI";
 
-import { initAdjustPage } from "./dom/adjustPageUI.js";
-
 async function fetchEntries() {
 	return guestState.isGuest ? getLocalEntries() : await getServerEntries();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
 	initMain();
-
-	setTimeout(() => {
-		initAdjustPage();
-	}, 10);
 });
 
 export { fetchEntries };
